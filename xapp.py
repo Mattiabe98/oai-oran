5a30b275297c97321ebe508e256b71364d45a517
Mattia <49247389+Mattiabe98@users.noreply.github.com>
2025-01-27 12:01:31 +0100

    Update xapp.py

--- xapp.py
+++ xapp.py
@@ -103,7 +103,7 @@ class GTPCallback(ric.gtp_cb):
             for id, stat in enumerate(ind.gtp_stats):
                 print('UE ID: ' + str(id))
                 print('GTP Tunnel ID: ' + str(stat.tunnel_id))
-                print('GTP QoS flow indicator: ' + str(stat.qfi))  # Example metric
+                #print('GTP QoS flow indicator: ' + str(stat.qfi))  # Example metric
                 #print('GTP gNB tunnel identifier: ' + str(stat.teidgnb))  # Example metric
 
 
